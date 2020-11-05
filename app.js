3e93476ddf5d008886070e7c213660dfe938b45b
Sara-bit-design <saradhingra3@gmail.com>
2020-11-05 19:44:01 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+app.get("/home" , function(req,res){
+	res.render("home");
+});
+
+app.get("/blog" , function(req,res){
+	res.render("blog");
+});
+
+app.listen(3000, function(req,res){
+	console.log("Server has started");
+});
